2ee5339376cb42a1b9c84df7faeec7858b8d26f1
Sthera30 <tinisthera@gmail.com>
2024-12-24 01:26:34 +0200

    cors changed

--- server/index.js
+++ server/index.js
@@ -32,10 +32,17 @@ const allowedOrigins = [
  }));
  */
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+ const corsOptions = {
+    origin: [
+        "http://localhost:5173", 
+        "https://fullstack-ordering-food-frontend-app.onrender.com" 
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    credentials: true 
+};
+
+
+app.use(cors(corsOptions));
 
 
 
